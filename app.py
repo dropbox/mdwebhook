d796bcd6e3d28de23e14e2676ed8d7fb0c07fa9f
Keigo Kanazawa <kkanazaw@yahoo-corp.jp>
2016-07-08 11:59:48 +0900

    dropbox api v2対応

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import threading
 import urlparse
 
-from dropbox.client import DropboxClient, DropboxOAuth2Flow
+import dropbox
 from flask import abort, Flask, redirect, render_template, request, session, url_for
 from markdown import markdown
 import redis
@@ -60,39 +60,41 @@ def oauth_callback():
 
     return redirect(url_for('done'))
 
-def process_user(uid):
+def process_user(account):
     '''Call /delta for the given user ID and process any changes.'''
 
     # OAuth token for the user
-    token = redis_client.hget('tokens', uid)
+    token = redis_client.hget('tokens', account)
 
     # /delta cursor for the user (None the first time)
-    cursor = redis_client.hget('cursors', uid)
+    cursor = redis_client.hget('cursors', account)
 
-    client = DropboxClient(token)
+    client = Dropbox(token)
     has_more = True
 
     while has_more:
-        result = client.delta(cursor)
+        if cursor is None:
+            result = dbx.files_list_folder(path='')
+        else:
+            result = dbx.files_list_folder_continue(cursor)
 
-        for path, metadata in result['entries']:
-
+        for entry in result.entries:
             # Ignore deleted files, folders, and non-markdown files
-            if (metadata is None or
-                    metadata['is_dir'] or
-                    not path.endswith('.md')):
+            if (isinstance(entry, DeletedMetadata) or
+                isinstance(entry, FolderMetadata) or
+                not entry.path_lower.endswith('.md')):
                 continue
-
             # Convert to Markdown and store as <basename>.html
-            html = markdown(client.get_file(path).read())
-            client.put_file(path[:-3] + '.html', html, overwrite=True)
-
+            _, resp = dbx.files_download(entry.path_lower)
+            html = markdown(resp.content)
+            dbx.files_upload(html, entry.path_lower[:-3] + '.html', mode=WriteMode('overwrite'))
+                                                                                        
         # Update cursor
-        cursor = result['cursor']
-        redis_client.hset('cursors', uid, cursor)
+        cursor = result.cursor
+        redis_client.hset('cursors', acount, cursor)
 
         # Repeat only if there's more to do
-        has_more = result['has_more']
+        has_more = result.has_more
 
 @app.route('/')
 def index():
@@ -126,12 +128,12 @@ def webhook():
     # Make sure this is a valid request from Dropbox
     if not validate_request(): abort(403)
 
-    for uid in json.loads(request.data)['delta']['users']:
+    for account in json.loads(request.data)['delta']['users']:
         # We need to respond quickly to the webhook request, so we do the
         # actual work in a separate thread. For more robustness, it's a
         # good idea to add the work to a reliable queue and process the queue
         # in a worker process.
-        threading.Thread(target=process_user, args=(uid,)).start()
+        threading.Thread(target=process_user, args=(account,)).start()
     return ''
 
 if __name__=='__main__':
